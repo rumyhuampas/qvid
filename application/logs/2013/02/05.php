31bc24ec93a413087777c666c4bd28974469a92b
rumyhuampas <rumyhuampas@gmail.com>
2013-02-06 01:29:05 +0000

    fotos principales desde la bd

--- application/logs/2013/02/05.php
+++ application/logs/2013/02/05.php
@@ -949,3 +949,17 @@
 #11 C:\wamp\www\qvid\system\classes\Kohana\Request.php(990): Kohana_Request_Client->execute(Object(Request))
 #12 C:\wamp\www\qvid\index.php(118): Kohana_Request->execute()
 #13 {main} in :
+2013-02-05 19:18:57 --- CRITICAL: Database_Exception [ 1054 ]: Unknown column 'createdAt' in 'order clause' [ SELECT `mediaresource`.`path`, `mediaresource`.`filename` FROM `mediaresource` WHERE `resource_type` = 'PICTURE' AND `filename` = 'HomeSlide' ORDER BY `createdAt` DESC ] ~ MODPATH\database\classes\Kohana\Database\MySQL.php [ 194 ] in C:\wamp\www\qvid\modules\database\classes\Kohana\Database\Query.php:251
+2013-02-05 19:18:57 --- DEBUG: #0 C:\wamp\www\qvid\modules\database\classes\Kohana\Database\Query.php(251): Kohana_Database_MySQL->query(1, 'SELECT `mediare...', false, Array)
+#1 C:\wamp\www\qvid\application\classes\helpers\db.php(82): Kohana_Database_Query->execute()
+#2 C:\wamp\www\qvid\application\views\home.php(12): helpers_db::getHomeSlides()
+#3 C:\wamp\www\qvid\system\classes\Kohana\View.php(61): include('C:\wamp\www\qvi...')
+#4 C:\wamp\www\qvid\system\classes\Kohana\View.php(348): Kohana_View::capture('C:\wamp\www\qvi...', Array)
+#5 C:\wamp\www\qvid\application\classes\Controller\home.php(10): Kohana_View->render()
+#6 C:\wamp\www\qvid\system\classes\Kohana\Controller.php(84): Controller_Home->action_index()
+#7 [internal function]: Kohana_Controller->execute()
+#8 C:\wamp\www\qvid\system\classes\Kohana\Request\Client\Internal.php(97): ReflectionMethod->invoke(Object(Controller_Home))
+#9 C:\wamp\www\qvid\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#10 C:\wamp\www\qvid\system\classes\Kohana\Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#11 C:\wamp\www\qvid\index.php(118): Kohana_Request->execute()
+#12 {main} in C:\wamp\www\qvid\modules\database\classes\Kohana\Database\Query.php:251
