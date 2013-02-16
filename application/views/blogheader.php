7c4d85002067fce41eb8a0e31548a2cdeb455ffb
rumyhuampas <rumyhuampas@gmail.com>
2013-02-16 20:28:50 +0000

    agregadas funcionalidades al blog

---
+++ application/views/blogheader.php
@@ -0,0 +1,19 @@
+<!-- 960 Container -->
+<div class="container floated">
+
+    <div class="sixteen floated page-title">
+
+	    <h2><?php echo $blogTitle ?></php></h2>
+
+	    <nav id="breadcrumbs">
+		    <ul>
+			    <li>Usted esta aqui:</li>
+			    <li><a href=<?php echo URL::base(); ?> >Inicio</a></li>
+			    <li>Blog</li>
+		    </ul>
+	    </nav>
+
+    </div>
+
+</div>
+<!-- 960 Container / End -->
--- application/views/blogreader.php
+++ application/views/blogreader.php
@@ -5,25 +5,7 @@
 <div id="content">
 
 
-    <!-- 960 Container -->
-    <div class="container floated">
-
-	    <div class="sixteen floated page-title">
-
-		    <h2>QVid Blog</h2>
-
-		    <nav id="breadcrumbs">
-			    <ul>
-				    <li>Usted esta aqui:</li>
-				    <li><a href=<?php echo URL::base(); ?> >Inicio</a></li>
-				    <li>Blog</li>
-			    </ul>
-		    </nav>
-
-	    </div>
-
-    </div>
-    <!-- 960 Container / End -->
+    <?php include Kohana::find_file('views', 'blogheader'); ?>
 
 
     <!-- 960 Container -->
@@ -36,46 +18,46 @@
 				if ($blog): 
 					$b=$blog[0]; ?>
 					<article class="post">
-					<?php $bMedia = helpers_db::getBlogResources($b['id']);
-					if(count($bMedia) > 0)
-					{
-					?>
-						<section class="flexslider">
-							<ul class="slides post-img">
-							<?php foreach ($bMedia as $bMed): ?> 
-							    <li style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;" class="">
-							    	<a href=<?php echo URL::base().$bMed['path'] ?> rel="fancybox-gallery" title=<?php echo $bMed['filename'] ?>>
-							    		<?php if($bMed['resource_type'] == 'PICTURE'){ ?>
-							    			<img src=<?php echo URL::base().$bMed['path'] ?> alt="">
-							    		<?php 
-										}
-										else
-										{?>
-											<div class="video"><iframe width="560" height="315" src=<?php echo $bMed['path']?> frameborder="0"></iframe></div>	
-										<?php 
-										} 
-										?>
-							    	</a>
-							    </li>
-							<?php endforeach ?>
-							</ul>
-							
-							<!--FLEX SLIDER -->
-							<?php 
-							if(count($bMedia) > 1)
-							{
-							?>
-						    	<ul class="flex-direction-nav">
-						    		<li><a class="flex-prev" href="#">Anterior</a></li>
-						    		<li><a class="flex-next" href="#">Siguiente</a></li>
-						    	</ul>
-						    <?php 
-						    }
-						    ?>
-				    	</section>
-			    	<?php
-					}
-			    	?>
+						<?php $bMedia = helpers_db::getBlogPictures($b['id']);
+						if(count($bMedia) > 0)
+						{
+						?>
+							<section class="flexslider">
+								<ul class="slides post-img">
+								<?php foreach ($bMedia as $bMed):?> 
+								    <li style="width: 100%; float: left; margin-right: -100%; position: relative; display: none;" class="">
+								    	<a href=<?php echo URL::base().$bMed['path'] ?> rel="fancybox-gallery" title=<?php echo $bMed['filename'] ?>>
+								    		<img src=<?php echo URL::base().$bMed['path'] ?> alt="">
+								    	</a>
+								    </li>
+							    <?php endforeach ?>
+								</ul>
+								
+								<!--FLEX SLIDER -->
+								<?php 
+								if(count($bMedia) > 1)
+								{
+								?>
+							    	<ul class="flex-direction-nav">
+							    		<li><a class="flex-prev" href="#">Anterior</a></li>
+							    		<li><a class="flex-next" href="#">Siguiente</a></li>
+							    	</ul>
+							    <?php 
+							    }
+							    ?>
+					    	</section>
+				    	<?php
+						}
+					 	$bVids = helpers_db::getBlogVideos($b['id']);
+						if(count($bVids) > 0)
+						{
+							foreach ($bVids as $bV): ?>
+						    	<a href=<?php echo URL::base().$bV['path'] ?> rel="fancybox-gallery" title=<?php echo $bV['filename'] ?>>
+					    			<div class="video"><iframe width="560" height="315" src=<?php echo $bV['path']?> frameborder="0"></iframe></div>
+						    	</a>
+							<?php endforeach;
+						}
+				    	?>
 		    
                     <section class="date">
 				        <span class="day"><?php echo $b['day']; ?></span>
@@ -102,11 +84,15 @@
 						<h4><?php echo $b['username']?></h4>
 						<p><?php echo $b['userdesc']?></p>
 					</div>
-				</section> 
+				</section>
+				
+				<article class="post">
+					<a href=<?php echo URL::base().Route::get('default')->uri(array('controller' => 'blog')); ?> class="button color">Volver al blog</a>
+				</article> 
                 
                 <div class="line"></div>
 			<?php endif ?>
-
+			
 	    </div>
 	    <!-- Content / End -->
 
